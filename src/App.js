bc0048209a99df89a9666f3e2c7ad2dc20af747a
unknown <racagnisasha@gmail.com>
2022-03-14 15:13:23 -0300

    created GlobalProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CartContext } from './context/CartContext';
-import { NotificationProvider } from './services/Notification/Notification';
 import NavBar from './components/NavBar/NavBar';
 import Slider from './components/Slider/Slider';
 import CategoryContainer from './components/CategoryContainer/CategoryContainer';
@@ -10,35 +8,31 @@ import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetail
 import Cart from './components/Cart/Cart';
 import ContactForm from './components/ContactForm/ContactForm';
 import Footer from './components/Footer/Footer';
-import SearchContext from './context/SearchContext';
+import GlobalProvider from './context/GlobalProvider';
 
 function App() {
 
   return (
     <div className="App">
-    <NotificationProvider>
-      <SearchContext>
-        <CartContext>
-          <BrowserRouter>
-            <NavBar brandName="iMarket" />
-            <Routes>
-              <Route path='/' element={
-                <>
-                  <Slider/>
-                  <CategoryContainer/> 
-                  <ItemListContainer/>
-                </>
-              }/>
-              <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
-              <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>
-              <Route path='/cart' element={<Cart/>}/>
-              <Route path='/order' element={<ContactForm/>}/>
-            </Routes>
-            <Footer/>
-          </BrowserRouter>
-        </CartContext>
-      </SearchContext>  
-    </NotificationProvider>  
+    <GlobalProvider>
+      <BrowserRouter>
+        <NavBar brandName="iMarket" />
+          <Routes>
+            <Route path='/' element={
+              <>
+                <Slider/>
+                <CategoryContainer/> 
+                <ItemListContainer/>
+              </>
+            }/>
+            <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
+            <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/order' element={<ContactForm/>}/>
+          </Routes>
+        <Footer/>
+      </BrowserRouter> 
+    </GlobalProvider>  
     </div>
   );
 }
