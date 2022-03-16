27bceb1d8ebadcb8bebdbbbeeb534b73bcb45c85
unknown <racagnisasha@gmail.com>
2022-03-16 08:10:53 -0300

    footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,10 +12,10 @@ const Footer = () => {
             </div>
         
             <div className='footer__column'>
-                <h2 className='footer__column--heading'>Politicas</h2>
+                <h2 className='footer__column--heading'>Políticas</h2>
                 <div className='footer__column--sites'>
-                    <Link to='/politica-de-privacidad'><p>Politicas de privacidad</p></Link>
-                    <Link to='/terminos-y-condiciones'><p>Terminos y condiciones</p></Link>
+                    <Link to='/politica-de-privacidad'><p>Políticas de privacidad</p></Link>
+                    <Link to='/terminos-y-condiciones'><p>Términos y condiciones</p></Link>
                 </div>
             </div>
         
@@ -23,12 +23,12 @@ const Footer = () => {
                 <h2 className='footer__column--heading'>Servicios al cliente</h2>
                 <div className='footer__column--sites'>
                     <Link to='/envios'><p>Envios</p></Link>
-                    <Link to='/contactanos'><p>Contactanos</p></Link>
+                    <Link to='/contactanos'><p>Contáctanos</p></Link>
                 </div>
             </div>
         
             <div className='footer__column'>
-                <h2 className='footer__column--heading'>Metodos de pago</h2>
+                <h2 className='footer__column--heading'>Métodos de pago</h2>
                 <img className='footer__column--img' src='https://www.sumaelectrohogar.com.ar/img/cms/medios-de-pago-suma(2).png' alt='metodos de pago'/>
             </div>
         </div>
